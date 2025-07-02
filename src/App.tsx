c121dc54fd2cb8bfd810fa64e0e5fb9e49e78a65
Anibal Mendoza <anibalmd32@gmail.com>
2025-07-02 07:51:56 -0400

    change browser router by hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { AppProvider, useApp } from './contexts/AppContext';
 import Header from './components/Layout/Header';
 import Sidebar from './components/Layout/Sidebar';
